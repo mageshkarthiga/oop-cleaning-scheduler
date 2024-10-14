d03c28f0995b04fe5f59db50c70981487e2920d3
mageshkarthiga <mageshk.2021@scis.smu.edu.sg>
2024-10-14 16:58:31 +0800

    feat: month and year

--- src/app/calendar/page.js
+++ src/app/calendar/page.js
@@ -1,54 +1,55 @@
 "use client";
-import React, { useState, useRef } from "react";
-import { TabMenu } from 'primereact/tabmenu';
+import React, { useState, useRef, useEffect } from "react";
 import { Button } from 'primereact/button';
 import Calendar from '@toast-ui/react-calendar';
 import '@toast-ui/calendar/dist/toastui-calendar.min.css';
 
 export default function CalendarPage() {
-    const items = [
-        { label: 'Calendar', icon: 'pi pi-calendar-clock' },
-        { label: 'MC Submitted', icon: 'pi pi-file-arrow-up' },
-        { label: 'Products', icon: 'pi pi-list' },
-        { label: 'Messages', icon: 'pi pi-inbox' }
-    ];
 
     const calendarRef = useRef();
     const [currentView, setCurrentView] = useState('month');
+    const [currentDate, setCurrentDate] = useState(new Date());
 
-    const monthTemplate = (date) => {
-        const monthName = date.toLocaleString('default', { month: 'long' });
-        const year = date.getFullYear();
-        return `<span>${monthName} ${year}</span>`;
-    }
+    useEffect(() => {
+        const calendarInstance = calendarRef.current.getInstance();
+        calendarInstance.on('afterRender', updateMonthYear);
+    }, []);
+
+    const updateMonthYear = () => {
+        const calendarInstance = calendarRef.current.getInstance();
+        const currentDate = calendarInstance.getDate();
+        setCurrentDate(currentDate);
+    };
 
     const handlePrevMonth = () => {
         const calendarInstance = calendarRef.current.getInstance();
         calendarInstance.prev();
+        updateMonthYear();
     };
 
     const handleNextMonth = () => {
         const calendarInstance = calendarRef.current.getInstance();
         calendarInstance.next();
+        updateMonthYear();
     };
 
     const handleChangeView = (viewType) => {
         setCurrentView(viewType);
         const calendarInstance = calendarRef.current.getInstance();
         calendarInstance.changeView(viewType, true);
-        console.log(calendarInstance.getOptions());
+        updateMonthYear();
+    };
+
+    const monthTemplate = (date) => {
+        return new Intl.DateTimeFormat('en-SG', { year: 'numeric', month: 'long' }).format(date);
     };
 
     return (
         <div>
-            <div className="container">
-                <TabMenu model={items} />
-            </div>
-            <br />
-            <div className="sm:container border-4">
-                <div className="flex justify-between mb-4">
+            <div className="sm:container border-4 mx-auto">
+                <div className="flex justify-between items-center mb-4">
                     <Button label="Previous Month" onClick={handlePrevMonth} icon="pi pi-chevron-left" severity="info" text />
-                    
+                    <h2 className="text-xl text-center font-bold">{monthTemplate(currentDate)}</h2>
                     <Button label="Next Month" onClick={handleNextMonth} icon="pi pi-chevron-right" severity="info" text />
                 </div>
                 <div className="flex justify-center space-x-4 mb-4">
@@ -61,7 +62,7 @@ export default function CalendarPage() {
                     view={currentView}
                     month={{
                         dayNames: ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'],
-                        title: monthTemplate
+                        isAlways6Weeks: false,
                     }}
                     calendars={[]} 
                     events={[]} 
